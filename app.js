a9c82cf4e0ee0e1954e310bcd1e3d1b2e3557d40
Punit <punitaggarwal2101@gmail.com>
2026-07-12 22:12:00 +0530

    Added index route for listing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/trekzo";
@@ -13,21 +14,29 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Hi i am root");
 });
 
-app.get("/listing", async (req, res) => {
-  let samplelisting = new Listing({
-    title: "My New Villa",
-    description: "By the beach",
-    price: 1200,
-    location: "Calangute ,Goa",
-    country: "India",
-  });
-  await samplelisting.save();
-  res.send("Done");
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+// app.get("/listing", async (req, res) => {
+//   let samplelisting = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute ,Goa",
+//     country: "India",
+//   });
+//   await samplelisting.save();
+//   res.send("Done");
+// });
 
 app.listen(8080, () => {
   console.log("server is listening to 8080");
